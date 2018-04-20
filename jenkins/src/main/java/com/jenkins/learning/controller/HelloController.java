8f152ee6d2c5bfc1802170e55c39ae40aef5df3d
Jay Garzon <jaygarzon@Jay-Garzons-MacBook-Pro.local>
2018-04-20 14:17:23 -0500

    added a rest controller and changed to port 8081

---
+++ jenkins/src/main/java/com/jenkins/learning/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.jenkins.learning.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String sayHello(){
+        return "HI";
+    }
+}
